00c5d3885e183cad9196948fa0caca1e4225d5b8
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-04-09 13:45:52 +0200

    Add service dependencies in Docker Compose and streamline MariaDB setup script

--- src/requirements/mariadb/tools/setup.sh
+++ src/requirements/mariadb/tools/setup.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-set -e
 
-sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+echo "Starting MariaDB service..."
+service mysql start
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB data directory..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" > temp.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> temp.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" >> temp.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> temp.sql
+echo "FLUSH PRIVILEGES;" >> temp.sql
 
-    echo "Starting MariaDB service..."
-    service mysql start
+mysql < temp.sql
 
-    echo "Creating database and user..."
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-    mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -e "FLUSH PRIVILEGES;"
-
-    echo "Stopping MariaDB service..."
-    service mysql stop
-else
-    echo "MariaDB data directory already initialized."
-fi
+rm temp.sql
 
 echo "Starting MariaDB in the foreground..."
-exec mysqld_safe
