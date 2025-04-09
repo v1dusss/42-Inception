fff595fa79728e6d522ba93f7c525b0b76e3c680
Vidus Sivanathan <148643381+v1dusss@users.noreply.github.com>
2025-04-09 14:54:51 +0200

    enhance setup script for WP-CLI configuration (wip)

--- src/requirements/wordpress/tools/setup.sh
+++ src/requirements/wordpress/tools/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pharchmod +x wp-cli.phar
+
+mv wp-cli.phar /user/local/bin/wp
+
+wp config create\
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost="mariadb" \
+    --path=/var/www/html/ \
+    --allow-root
